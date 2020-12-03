4476c347935a2a233505c69dfab397e025d30500
NandoRunner <nando.az@gmail.com>
2020-12-03 16:17:01 +0000

    CSS not loading - attempt 11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -77,8 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.serve_static_assets = true
-
-  config.assets.compress = true
 end
